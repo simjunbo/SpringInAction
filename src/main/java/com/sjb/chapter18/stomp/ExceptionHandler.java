32e142317491d8116ec6cf5617daec61aa927569
simjunbo <s860620@gmail.com>
2018-08-08 23:14:26 +0900

    core

--- src/main/java/com/sjb/chapter18/stomp/ExceptionHandler.java
+++ src/main/java/com/sjb/chapter18/stomp/ExceptionHandler.java
@@ -1,7 +1,14 @@
 package com.sjb.chapter18.stomp;
 
+import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
+
 /**
  * Created by simjunbo on 2018-08-07.
  */
 public class ExceptionHandler {
+
+    @MessageExceptionHandler
+    public void handleException(Throwable t) {
+
+    }
 }
--- src/main/java/com/sjb/chapter18/stomp/MarcoController.java
+++ src/main/java/com/sjb/chapter18/stomp/MarcoController.java
@@ -3,6 +3,8 @@ package com.sjb.chapter18.stomp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -15,7 +17,20 @@ public class MarcoController {
             LoggerFactory.getLogger(MarcoController.class);
 
     @MessageMapping("/marco")
-    public void handleShout(Shout incoming) {
+    @SendTo("/topic/shout")
+    public Shout handleShout(Shout incoming) {
         logger.info("Received message: " + incoming.getMessage());
+
+        Shout outgoing = new Shout();
+        outgoing.setMessage("Polo!");
+        return outgoing;
     }
+
+    @SubscribeMapping({"/marco"})
+    public Shout handleSubscription() {
+        Shout outgoing = new Shout();
+        outgoing.setMessage("Polo!");
+        return outgoing;
+    }
+
 }
